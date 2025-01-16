ce0de68ce778ae380bd17e610a8f0c4669a2955a
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-01-16 12:01:56 +0800

    update code

--- set.sh
+++ set.sh
@@ -2,3 +2,16 @@ apt-get update
 apt-get install libcairo2-dev
 pip install Pillow wandb matplotlib cairosvg ftfy tensorboard Jinja2 peft sentencepiece protobuf
 pip install --upgrade diffusers accelerate transformers xformers torchvision datasets deepspeed
+
+# Set Up for DreamBooth Training
+cd external
+git clone https://github.com/huggingface/diffusers.git
+cd diffusers
+pip install -e .
+pip install wandb
+pip install backports.tarfile
+cd examples/dreambooth
+pip install -r requirements.txt
+
+accelerate config
+wandb login
---
+++ train_dreambooth.sh
@@ -0,0 +1,24 @@
+export MODEL_NAME="CompVis/stable-diffusion-v1-4"
+export INSTANCE_DIR="data/kanji_image"
+export OUTPUT_DIR="checkpoint/kanji_db"
+
+# Validation image stored in WandB 
+
+accelerate launch ../train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --instance_prompt="an image of sks character" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=400 \
+  --mixed_precision="no" \
+  --report_to="wandb" \
+  --logging_dir="log" \
+  --validation_prompt="an image of sks character meaning 'dog'" \
+  --num_validation_images=4 \
+  --validation_steps=100 \
