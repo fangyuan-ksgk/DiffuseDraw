917baf9822bb21a0cb6b5792f40137bee31c5112
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-01-19 00:08:46 +0800

    update script

--- prepare_data.py
+++ prepare_data.py
@@ -1,3 +1,12 @@
-from src.utils import prepare_kanji_dict
+# from src.utils import prepare_kanji_dict
 
-prepare_kanji_dict(regenerate=True)
+# prepare_kanji_dict(regenerate=True)
+
+
+from datasets import load_dataset
+from tqdm import tqdm
+# load dataset
+dataset = load_dataset("Ksgk-fy/expanded-kanji-dataset")
+# save images
+for idx, img in enumerate(tqdm(dataset['train']['image'])):
+    img.save(f"data/kanji_expand_image/kanji_{idx}.png")
