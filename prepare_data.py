7f1229d63d100b18f740eba8fc610f58b026869c
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-01-20 01:04:22 +0800

    update code

--- prepare_data.py
+++ prepare_data.py
@@ -5,8 +5,15 @@
 
 from datasets import load_dataset
 from tqdm import tqdm
+import os 
+os.makedirs("data/kanji_expand_image", exist_ok=True)
+
 # load dataset
 dataset = load_dataset("Ksgk-fy/expanded-kanji-dataset")
 # save images
 for idx, img in enumerate(tqdm(dataset['train']['image'])):
     img.save(f"data/kanji_expand_image/kanji_{idx}.png")
+    
+# Pipeline of Training (Conceptual)
+# - 1. DreamBooth Concept Embedding 
+# - 2. Text2Image Caption-Image Matching
