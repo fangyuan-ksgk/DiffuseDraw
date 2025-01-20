f1026da06df5afbbecf208b9bd2c456c0aa9aae6
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-01-20 11:24:55 +0800

    update train_inception script

--- src/train_inception.py
+++ src/train_inception.py
@@ -1,17 +1,5 @@
-#!/usr/bin/env python
-# coding=utf-8
-# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
+# @Fangyuan Yu 
+# Modified from HuggingFace Diffusers, Apache 2.0 License
 
 import argparse
 import copy
@@ -1484,7 +1472,7 @@ def main(args):
                 images=images,
                 base_model=args.pretrained_model_name_or_path,
                 train_text_encoder=args.train_text_encoder,
-                prompt=args.instance_prompt,
+                prompt=args.concept_prompt, # potential issue
                 repo_folder=args.output_dir,
                 pipeline=pipeline,
             )
